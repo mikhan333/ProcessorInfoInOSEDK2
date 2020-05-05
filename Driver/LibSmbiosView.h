0ce794b5cdfd3ed7ad6ccf26ef2c774438b45e2e
mikhan333 <mikhan333@mail.ru>
2020-05-05 17:50:50 +0300

    Delete some comments

--- Driver/LibSmbiosView.h
+++ Driver/LibSmbiosView.h
@@ -1,17 +1,3 @@
-/** @file
-  API for SMBIOS Plug and Play functions, access to SMBIOS table and structures.
-
-  Copyright (c) 2005 - 2015, Intel Corporation. All rights reserved.<BR>
-  This program and the accompanying materials
-  are licensed and made available under the terms and conditions of the BSD License
-  which accompanies this distribution.  The full text of the license may be found at
-  http://opensource.org/licenses/bsd-license.php
-
-  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
-  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
-
-**/
-
 #ifndef _LIB_SMBIOS_VIEW_H_
 #define _LIB_SMBIOS_VIEW_H_
 
